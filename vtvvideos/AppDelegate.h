cff2ca8ab9ae9a07851746e7fe15e2eb8e86a847
Dao Duy Quang <daoduyquang91@gmail.com>
2016-02-21 18:58:42 +0700

    Initial Commit

---
+++ vtvvideos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vtvvideos
+//
+//  Created by Duy Quang on 2/21/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ vtvvideos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vtvvideos
+//
+//  Created by Duy Quang on 2/21/16.
+//  Copyright © 2016 duyquang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
